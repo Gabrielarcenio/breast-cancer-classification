d4c1d977e6d50996fe6610acf0093a0cf7183557
Gabriel Arcenio <103552115+Gabrielarcenio@users.noreply.github.com>
2024-07-15 14:23:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,71 @@
+#Importando as Bibliotecas Necessárias
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.linear_model import Perceptron
 from google.colab import drive
-
-# Montando o Google Drive na mesma conta do Google Colab
-drive.mount('/content/drive')
-
-# Carregando o Dataset
+drive.mount('/content/drive') # Montando o Google Drive na mesma conta do Google Colab
+#Carregando o Dataset
+# Descrição do dataset: https://www.kaggle.com/datasets/uciml/breast-cancer-wisconsin-data
+# Caminho do dataset no Google Drive que será carregado em df
 df = pd.read_csv("/content/drive/MyDrive/RNA_Datasets/BreastCancerWisconsinDataSet.csv")
-
-# Explorando o Dataset
-print(df.head())
-print(df.tail())
-print(df.info())
-print(df.describe())
-print(df.shape)
-print(df.isnull().sum())
-
-# Pré-Processando os Dados
+#Explorando o Dataset
+# Diagnosis (M = malignant, B = benign)
+df.head()
+df.tail()
+df.info()
+df.describe()
+df.shape
+df.isnull().sum()
+#Pré-Processando os Dados
+# Excluindo as variáveis id e Unnamed, pois são irrelevantes no contexto de informações
 df.drop(columns=['id', 'Unnamed: 32'], inplace=True)
-print(df.shape)
+df.shape
 
+# Codificando a coluna de destino usando o codificador de rótulo - Transforma em 0 e 1 as Classes - Diagnosis (M = malignant = 1, B = benign = 0)
 encoder = LabelEncoder()
 df['diagnosis'] = encoder.fit_transform(df['diagnosis'])
 
 X = df.drop(columns=['diagnosis'])
 y = df['diagnosis']
 
+# Normalizando os Dados
 scaler = MinMaxScaler()
 X = scaler.fit_transform(X)
 
-# Plotando o gráfico para verificação da distribuição amostral das variáveis do dataset
-X_plot = df.iloc[:,:].values  # transformando o df em ndarray para usar no scatter
-plt.scatter(X_plot[:,2], X_plot[:,3], c=y)
-plt.title("Distribuição do Diagnóstico por Variáveis de Entrada")
-plt.xlabel('Var 1')
-plt.ylabel('Var 2')
-plt.show()
+# plotando o gráfico para verificação da distribuição amostral das variáveis do dataset
+X = df.iloc[:,:].values # transformando o df em ndarray para usar no scatter
+pl = plt
+pl.scatter(X[:,2],X[:,3],c=y)
+pl.title("Distribuição do Diagnóstico por Variáveis de Entrada" )
+pl.xlabel('Var 1')
+pl.ylabel('Var 2')
+pl.show
 
-# Separando os Dados para Treinamento e Teste
+#Separando os Dados para Treinamento e Teste
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Construindo e Testando o Modelo
+#Construindo e Testando o Modelo
 p = Perceptron()
 p.fit(X_train, y_train)
 y_pred = p.predict(X_test)
 
-# Confusion Matrix
 cm = confusion_matrix(y_test, y_pred)
 sns.heatmap(cm, annot=True)
 plt.show()
 
-# Apresentação das Métricas dos Testes Validados
+#Apresentação das Métricas dos Testes Validados
 test_score = accuracy_score(y_pred, y_test)
 print("Acurácia dos testes: ", test_score)
+
 print(classification_report(y_pred, y_test))
+
 print("Número de épocas no treinamento: ", p.n_iter_)
 print("Lista de parâmetros configurados na Perceptron: ", p.get_params())
+
+
